7eee030503ec2cea5540593319ff5ecb59b12a9c
divyatejapurnknti <30713250+divyatejapurnknti@users.noreply.github.com>
2017-09-08 12:55:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,8 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text')
-;
-element.innerHTML='New value';
-var img=document.getElementById('madi');
-var marginleft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    img.style.marginleft='100px';
+var button=document.getElementById('counter';
+var counter=0;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
     
-};
+}
