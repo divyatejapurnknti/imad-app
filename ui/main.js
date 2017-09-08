e8ea18f43d379d1a5a141f4c4c6bde91da8bc553
divyatejapurnknti <30713250+divyatejapurnknti@users.noreply.github.com>
2017-09-08 11:57:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ var element=document.getElementById('main-text')
 ;
 element.innerHTML='New value';
 var img=document.getElementById('img');
+var marginleft=0;
+function moveright(){
+    marginleft-margin+10;
+    img.style.margin-marginleft+px;
+}
 img.onclick=function(){
     img.style.marginleft='100px';
     
